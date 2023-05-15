c49b3e2ee894fa0ff76ae12544c6aa428b4c2027
ahsn.dev <ahsn.dev@gmail.com>
2023-05-15 18:21:38 +0330

    Fix the issue with Chakra menus

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,10 @@ interface Props {
 
 const GameGrid = ({ gameQurey }: Props) => {
   const { data, error, isLoading } = useGames(gameQurey);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28,
+  ];
 
   return (
     <>
